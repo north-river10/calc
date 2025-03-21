bd8c9c5ba3a9533e59db689ccf723c79397dedc0
north-river10 <seidaikitagawa@gmail.com>
2025-03-21 17:51:30 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 class Calculator {
-  constructor(previousOperantand Text)
+  constructor(previousOperantTextElement,currentOperantTextElement){
+    this.
 }
 
 const numbButtons=document.querySelectorAll('[data-number]')
-const numbButtons=document.querySelectorAll('[data-operation]')
-const numbButtons=document.querySelectorAll('[data-equals]')
+const operationButtons=document.querySelectorAll('[data-operation]')
+const equalsButton=document.querySelector('[data-equals]')
+const deleteButton=document.querySelector('[data-delete]')
+const allClearButton=document.querySelector('[data-all-clear]')
+const previousOperantTextElement=document.querySelector('[data-previous-operant]')
+const currentOperantTextElement=document.querySelector('[data-current-operant]')
