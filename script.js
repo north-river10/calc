4ab5bd65e14e1f3e9463ceb0b2686c0f1a25b385
north-river10 <seidaikitagawa@gmail.com>
2025-03-21 17:40:57 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+class Calculator {
+  constructor(previousOperantand Text)
+}
+
+const numbButtons=document.querySelectorAll('[data-number]')
+const numbButtons=document.querySelectorAll('[data-operation]')
+const numbButtons=document.querySelectorAll('[data-equals]')
